4498bf52d51240051fbf58a6c04c017b390ea91f
andrey <andreyb@lombard.finance>
2025-08-29 17:36:55 +0300

    * fix formatting

--- contracts/mocks/TokenDistributorMock.sol
+++ contracts/mocks/TokenDistributorMock.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.28;
 import "../BARD/TokenDistributor.sol";
 
 contract TokenDistributorMock is TokenDistributor {
-
     constructor(
         bytes32 _merkleRoot,
         address _token,
@@ -13,9 +12,17 @@ contract TokenDistributorMock is TokenDistributor {
         address _vault,
         address _pauser,
         address _approver
-    ) TokenDistributor(_merkleRoot, _token, _owner, _claimEnd, _vault, _pauser, _approver) {
-
-    }
+    )
+        TokenDistributor(
+            _merkleRoot,
+            _token,
+            _owner,
+            _claimEnd,
+            _vault,
+            _pauser,
+            _approver
+        )
+    {}
 
     function removeClaim(address _account) external whenNotPaused {
         hasClaimed[_account] = false;
